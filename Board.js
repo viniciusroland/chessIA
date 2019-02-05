ddcfa7797e562791c5f22d2263496f34e3777ebd
viniciusroland <vini_roland4.10@hotmail.com>
2019-02-05 18:15:04 -0200

    track of moved pieces on the board

--- Board.js
+++ Board.js
@@ -22,4 +22,31 @@ class Board {
 
         }
     }
+    move_to_new_position(x, y, piece) {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (x > j * 80 && x < j * 80 + 80 && y > i * 80 && y < i * 80 + 80) {
+                    var posicao_nova = [i, j]
+                }
+            }
+        }
+
+        console.log('posicao_nova', posicao_nova)
+        this.board[posicao_nova[0]][posicao_nova[1]] = piece
+        console.log(this.board)
+    }   
+
+    remove_from_old_position(x, y) {
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (x >= j * 80 && x <= j * 80 + 80 && y >= i * 80 && y <= i * 80 + 80) {
+                    var posicao_antiga = [i, j]
+                }
+            }
+        }
+
+        console.log('posicao_antiga', posicao_antiga)
+        this.board[posicao_antiga[0]][posicao_antiga[1]] = 0
+        console.log(this.board)
+    }   
 }
