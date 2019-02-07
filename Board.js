be68145bb2c4920d147b5c2813d29cd5f777ed46
viniciusroland <vini_roland4.10@hotmail.com>
2019-02-07 14:45:50 -0200

    move rules to pawns

--- Board.js
+++ Board.js
@@ -1,7 +1,3 @@
-var new_position;
-var old_position;
-var white_pawn_first_move = false
-var black_pawn_first_move = false
 class Board {
 
     constructor(x, y, tabuleiro) {
@@ -18,104 +14,14 @@ class Board {
                 } else {
                     fill(0, 200, 0)
                 }
-                
                 rect(80 * i, 80 * j, 80, 80);
-
             }
-
         }
     }
 
-    move_to_new_position(x, y, piece) {
-        //mover esse metodo pras classes de cada peca e nao deixa-lo aqui no Board.js
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                if (x > j * 80 && x < j * 80 + 80 && y > i * 80 && y < i * 80 + 80) {
-                    var posicao_nova = createVector(i, j)
-                    new_position = posicao_nova
-                }
-            }
-        }
-
-        if(piece.name == "Pawn") {
-            if(piece.color == "black") {
-                if(new_position.y == old_position.y && new_position.x - old_position.x == -1) {
-                    console.log('preto andou uma casa')
-                    piece.first_move = true
-                } else if(!piece.first_move && new_position.x - old_position.x == -2){
-                    piece.first_move = true
-                    console.warn('preto andou 2 casa')
-                } else {
-                    console.error('ERROU')
-                }
-            } else if(piece.color == "white") {
-                if(new_position.y == old_position.y && new_position.x - old_position.x == 1) {
-                    console.log('branco andou uma casa')
-                    piece.first_move = true
-                } else if(!piece.first_move && new_position.x - old_position.x == 2){
-                    piece.first_move = true
-                    console.warn('branco andou 2 casa')
-                } else {
-                    console.error('ERROU')
-                }
-            }
-        }
-        console.log(piece.name, piece.color, new_position, old_position)
-        this.eat_pieces(posicao_nova)
-        piece.board_coords = posicao_nova
-        this.board[posicao_nova.x][posicao_nova.y] = piece
-        console.log('Tabuleiro atual:', this.board)
-        return posicao_nova;
-    }   
-
-    remove_from_old_position(x, y) {
-        //mover esse metodo pras classes de cada peca e nao deixa-lo aqui no Board.js
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                if (x >= j * 80 && x <= j * 80 + 80 && y >= i * 80 && y <= i * 80 + 80) {
-                    var posicao_antiga = createVector(i, j) 
-                    old_position = posicao_antiga
-                }
-            }
-        }
-
-        this.board[posicao_antiga.x][posicao_antiga.y] = 0
-        return posicao_antiga;
-    }   
-
-
-    eat_pieces(at_position) {
-        let piece_in_the_position = this.board[at_position.x][at_position.y]
-        this.board[at_position.x][at_position.y] = 0
-        if(piece_in_the_position != 0) {
-            //deleting piece object in the board table (this.board)
-            //this.board[at_position.x][at_position.y] = 0
-            if(piece_in_the_position.name == 'Pawn') {
-                removeAr(white_pawns, piece_in_the_position)
-                removeAr(black_pawns, piece_in_the_position)
-
-            } else if(piece_in_the_position.name == 'Tower') {
-                removeAr(white_towers, piece_in_the_position)
-                removeAr(black_towers, piece_in_the_position)
-             
-            } else if(piece_in_the_position.name == 'Horse') {
-                removeAr(white_horses, piece_in_the_position)
-                removeAr(black_horses, piece_in_the_position)
-
-            } else if(piece_in_the_position.name == 'Bishop') {
-                removeAr(white_bishops, piece_in_the_position)
-                removeAr(black_bishops, piece_in_the_position)
-
-            } else if(piece_in_the_position.name == 'Queen') {
-                removeAr(white_queens, piece_in_the_position)
-                removeAr(black_queens, piece_in_the_position)
-
-            } else if(piece_in_the_position.name == 'King') {
-                removeAr(white_kings, piece_in_the_position)
-                removeAr(black_kings, piece_in_the_position)
-            }
-
-        }
+    updateBoard(old_position, new_position, piece) {
+        this.board[old_position.x][old_position.y] = 0
+        this.board[new_position.x][new_position.y] = piece
     }
     
 }
--- Pawn.js
+++ Pawn.js
@@ -11,6 +11,7 @@ class Pawn {
         this.board_coords;
         this.color = color
         this.first_move = false
+        this.erro = false
     }
     display() {
          if(!this.clicado) {
@@ -32,29 +33,158 @@ class Pawn {
             this.clicado = true
             this.contador++
             if(this.contador == 2) {
-                //move peca para posicao nova
-                this.move_piece()
+                //tenta mover peca para posicao nova
+                this.check_move_rules()
             }
         } else if(this.contador == 1) {
-            //volta peca para posicao inicial 
-            this.move_piece()
+            //volta peca para posicao inicial caso usuario clique na mesma casa que estava para refazer a jogada
+            this.check_move_rules()
         }
     }
 
 
-    move_piece() {
-        old_position = board.remove_from_old_position(this.x, this.y)
-        this.x = mouseX - 25
-        this.y = mouseY - 25
-        new_position = board.move_to_new_position(mouseX, mouseY, this) 
-
-        if(rodada == 'white' && this.contador != 2) {
+    update_round() {
+        if(rodada == 'white' && this.contador != 2 && !this.erro) {
             rodada = 'black'
-        } else if(rodada == 'black' && this.contador != 2) {
+        } else if(rodada == 'black' && this.contador != 2 && !this.erro) {
             rodada = 'white'
+        }
+    }
+
+    check_move_rules() {
+        old_position = this.get_old_position(this.x, this.y)
+        new_position = this.get_new_position(mouseX, mouseY) 
+
+        if(this.name == "Pawn") {
+            if(this.color == "black") {
+                if(new_position.y == old_position.y && new_position.x - old_position.x == -1 && board.board[new_position.x][new_position.y] == 0) {
+                    //preto andou uma casa
+                    this.first_move = true
+                    this.eat_pieces(new_position)
+                    board.updateBoard(old_position, new_position, this)
+                    this.board_coords = new_position
+                    this.update_round()
+                    this.x = mouseX - 25
+                    this.y = mouseY - 25
+                } else if(!this.first_move && new_position.x - old_position.x == -2 && board.board[new_position.x][new_position.y] == 0){
+                    //preto andou duas casas
+                    this.first_move = true
+                    this.eat_pieces(new_position)
+                    board.updateBoard(old_position, new_position, this)
+                    this.board_coords = new_position
+                    this.update_round()
+                    this.x = mouseX - 25
+                    this.y = mouseY - 25
+                } else if(((new_position.x - old_position.x == -1 && new_position.y - old_position.y == -1) || (new_position.x - old_position.x == -1 && new_position.y - old_position.y == 1)) && (board.board[new_position.x][new_position.y] != 0 && board.board[new_position.x][new_position.y].color != this.color)){
+                    //comendo diagonalmente
+                    this.first_move = true
+                    this.eat_pieces(new_position)
+                    board.updateBoard(old_position, new_position, this)
+                    this.board_coords = new_position
+                    this.update_round()
+                    this.x = mouseX - 25
+                    this.y = mouseY - 25
+                } else {
+                    //errou
+                    console.error('ERROU')
+                }
+
+            } else if(this.color == "white") {
+                if(new_position.y == old_position.y && new_position.x - old_position.x == 1 && board.board[new_position.x][new_position.y] == 0) {
+                    //branco andou uma casa
+                    this.first_move = true
+                    this.eat_pieces(new_position)
+                    board.updateBoard(old_position, new_position, this)
+                    this.board_coords = new_position
+                    this.update_round()
+                    this.x = mouseX - 25
+                    this.y = mouseY - 25
+                } else if(!this.first_move && new_position.x - old_position.x == 2 && board.board[new_position.x][new_position.y] == 0){
+                    //branco andou duas casas
+                    this.first_move = true
+                    this.eat_pieces(new_position)
+                    board.updateBoard(old_position, new_position, this)
+                    this.board_coords = new_position
+                    this.update_round()
+                    this.x = mouseX - 25
+                    this.y = mouseY - 25
+                } else if(((new_position.x - old_position.x == 1 && new_position.y - old_position.y == 1) || (new_position.x - old_position.x == 1 && new_position.y - old_position.y == -1)) && (board.board[new_position.x][new_position.y] != 0 && board.board[new_position.x][new_position.y].color != this.color)){
+                    //comendo diagonalmente
+                    this.first_move = true
+                    this.eat_pieces(new_position)
+                    board.updateBoard(old_position, new_position, this)
+                    this.board_coords = new_position
+                    this.update_round()
+                    this.x = mouseX - 25
+                    this.y = mouseY - 25
+                } else {
+                    //errou
+                    console.error('ERROU')
+                }
+            }
         }
         this.contador = 0
         this.clicado = false
+        console.log('Tabuleiro atual:', board.board)
     }
+
+    get_old_position(x, y) {
+        //mover esse metodo pras classes de cada peca e nao deixa-lo aqui no Board.js
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (x >= j * 80 && x <= j * 80 + 80 && y >= i * 80 && y <= i * 80 + 80) {
+                    var posicao_antiga = createVector(i, j) 
+                }
+            }
+        }
+        return posicao_antiga;
+    }   
+
+    get_new_position(x, y) {
+        //mover esse metodo pras classes de cada peca e nao deixa-lo aqui no Board.js
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                if (x > j * 80 && x < j * 80 + 80 && y > i * 80 && y < i * 80 + 80) {
+                    var posicao_nova = createVector(i, j)
+                }
+            }
+        }
+        return posicao_nova;
+
+    }   
+
+    eat_pieces(at_position) {
+        let piece_in_the_position = board.board[at_position.x][at_position.y]
+        if(piece_in_the_position != 0 && piece_in_the_position.color != this.color) {
+            //deleting piece object in the board table (this.board)
+            //this.board[at_position.x][at_position.y] = 0
+            if(piece_in_the_position.name == 'Pawn') {
+                removeAr(white_pawns, piece_in_the_position)
+                removeAr(black_pawns, piece_in_the_position)
+
+            } else if(piece_in_the_position.name == 'Tower') {
+                removeAr(white_towers, piece_in_the_position)
+                removeAr(black_towers, piece_in_the_position)
+             
+            } else if(piece_in_the_position.name == 'Horse') {
+                removeAr(white_horses, piece_in_the_position)
+                removeAr(black_horses, piece_in_the_position)
+
+            } else if(piece_in_the_position.name == 'Bishop') {
+                removeAr(white_bishops, piece_in_the_position)
+                removeAr(black_bishops, piece_in_the_position)
+
+            } else if(piece_in_the_position.name == 'Queen') {
+                removeAr(white_queens, piece_in_the_position)
+                removeAr(black_queens, piece_in_the_position)
+
+            } else if(piece_in_the_position.name == 'King') {
+                removeAr(white_kings, piece_in_the_position)
+                removeAr(black_kings, piece_in_the_position)
+            }
+
+        }
+    }
+
 }
 
